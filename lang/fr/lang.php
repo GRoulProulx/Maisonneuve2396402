b976ae44371dac3501e7f1364d31936b4801771e
GRoulProulx <gproulx85@gmail.com>
2025-03-27 15:39:36 -0400

    Added french traduction files

---
+++ lang/fr/lang.php
@@ -0,0 +1,38 @@
+<?php
+//FR<?php
+
+return [
+    "text_welcome" => "Bienvenue à Chirps",
+    "text_welcome_paragraph" => "Une plateforme conçue pour les étudiants du Collège Maisonneuve pour se connecter, partager des ressources et se faire des amis",
+    "text_primary_btn" => "Commencer",
+    "text_secondary_btn" => "En savoir plus",
+    "list_students" => "Liste des étudiants",
+    "users" => "Utilisateurs",
+    "chirping" => "Commencer à gazouiller",
+    "french" => "Français",
+    "english" => "Anglais",
+    "languages" => "Langue",
+    "login" => "Connexion",
+    "logout" => "Déconnexion",
+    "register" => "S'inscrire",
+    "offer" => "Ce que nous offrons",
+    "community" => "Communauté",
+    "resources" => "Ressources",
+    "communication" => "Communication",
+    "news_letter" => "Prêt à nous rejoindre?",
+    "news_letter_paragraph" => "Inscrivez-vous aujourd'hui et faites partie de notre communauté grandissante.",
+    "news_letter_btn" => "S'inscrire maintenant",
+    "footer_text" => "Tous droits réservés.",
+    "footer_socials" => "Suivez-nous sur les réseaux sociaux:",
+    "liststudents" => "Liste des étudiants",
+
+    // /student
+    "addstudents" => "Ajouter un nouvel étudiant",
+    "name" => "Nom",
+    "address" => "Adresse",
+    "phone" => "Téléphone",
+    "email" => "Courriel",
+    "birthday" => "Date de naissance",
+    "city" => "Ville",
+    "actions" => "Actions",
+];
